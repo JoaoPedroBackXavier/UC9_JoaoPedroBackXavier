3b72687641c4998105a2e6962c5da5e393a47b87
JOAO PEDRO BACK XAVIER <04821428040@senacrs.edu.br>
2025-04-01 17:22:08 -0300

    commit

---
+++ bancodedados/src/main/java/com/mycompany/bancodedados/Bancodedados.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancodedados;
+
+/**
+ *
+ * @author JOAOPEDROBACKXAVIER
+ */
+import java.sql.Connection;
+
+public class Bancodedados {
+    public static void main(String[] args) {
+ //       ConexaoSQLite conexaoSQLite = new ConexaoSQLite();
+ //       Connection conexao = conexaoSQLite.conectar();
+//   ^     
+    //      CriarTabela.criarTabelaUsuarios(conexao);    
+   //       InserirUsuario.inserirUsuario(conexao, "joao", "joaozito.gameplays@gmail");
+  //  AtualizarUsuario.atualizarUsuarios(conexao, 1, "degustadorDeBostaLiquida69", "eu@gmail");
+ //   DeletarUsuario.deletarUsuario(conexao, 1);
+ //           ListarUsuarios.listarUsuarios(conexao);
+ 
+}
+}
